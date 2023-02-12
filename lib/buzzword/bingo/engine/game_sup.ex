b849a0e12e6d0d4f106f34b54d940c6284ce0aa5
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-12 05:16:31 -0500

    0.1.26

--- lib/buzzword/bingo/engine/game_sup.ex
+++ lib/buzzword/bingo/engine/game_sup.ex
@@ -10,8 +10,7 @@ defmodule Buzzword.Bingo.Engine.GameSup do
 
   ## Callbacks
 
-  @spec init(term) ::
-          {:ok, {:supervisor.sup_flags(), [:supervisor.child_spec()]}} | :ignore
+  @spec init(term) :: {:ok, {Supervisor.sup_flags(), [Supervisor.child_spec()]}}
   def init(:ok = _init_arg) do
     [
       # Child spec relying on `use DynamicSupervisor`...
